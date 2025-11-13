5aaa6c44c82cb765190031976506a38daeac53d0
ayasource <ayana-aac@hotmail.co.uk>
2025-11-13 10:57:30 +0000

    Add code for division game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ if (gameType === "addition") {
 displayMultiplyQuestion(num1, num2); 
 } else if (gameType === "subtract"){
   displaySubtractQuestion(num1, num2);  
+} else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
 } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -88,6 +90,10 @@ function calculateCorrectAnswer() {
 
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -120,6 +126,7 @@ document.getElementById(`operator`).textContent = "+";
 }
 
 function displaySubtractQuestion(operand1, operand2) {
+
 document.getElementById(`operand1`).textContent = operand1 > operand2 ? operand1 : operand2;
 document.getElementById(`operand2`).textContent = operand1 > operand2 ? operand2 : operand1;
 document.getElementById(`operator`).textContent = "-";
@@ -133,4 +140,11 @@ document.getElementById(`operator`).textContent = "x";
 
 }
 
+function displayDivisionQuestion(operand1, operand2) {
 
+operand1 = operand1 * operand2;
+document.getElementById(`operand1`).textContent = operand1;
+document.getElementById(`operand2`).textContent = operand2;
+document.getElementById(`operator`).textContent = "/";
+
+}
