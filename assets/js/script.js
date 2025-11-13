00df6ddcd9535cf7c3504aa99ad643e5f928aace
ayasource <ayana-aac@hotmail.co.uk>
2025-11-13 08:19:28 +0000

    Add for multiplication game code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ let num2 = Math.floor(Math.random() * 25) + 1;
 
 if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
+} else if(gameType === "multiply") { 
+displayMultiplyQuestion(num1, num2); 
 } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -69,7 +71,10 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {al
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -104,7 +109,11 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    
+document.getElementById(`operand1`).textContent = operand1;
+document.getElementById(`operand2`).textContent = operand2;
+document.getElementById(`operator`).textContent = "x";
 
 }
 
